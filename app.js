833dd056758a31b873230d63ad70550734926164
SARAN ST <saran.st@viewzenlabs.com>
2026-05-13 10:27:13 +0530

    added scada toggle function

--- app.js
+++ app.js
@@ -6,27 +6,39 @@ const app = express();
 const cm = new ControllerManager();
 const plc = cm.addController(plcConfig.ip, plcConfig.slot, plcConfig.polling_interval);     
 
-const allTags = [...plcConfig.tags.scale_in, ...plcConfig.tags.scale_out];
+const allTags = [...plcConfig.tags.scale_in];
 allTags.forEach(tagName => {
     plc.addTag(tagName);
 });
 
-//event based logging 
+    //event based logging 
 plc.on("TagChanged", (tag, prevValue) => {
     console.log(`[${new Date().toLocaleTimeString()}] CHANGE DETECTED:`);
     console.log(`   Tag:   ${tag.name}`);
     console.log(`   Value: ${prevValue} -> ${tag.value}`);
-
-    if (tag.name === "Scale_In_Data_Transfer_Bit" && tag.value === true) {
-        console.log(">>> Processing Scale In Record...");
-    }
 });
 
 plc.on("Connected", () => console.log(`Connected to PLC at ${plcConfig.ip}`));
-plc.on("Disconnected", () => console.warn("Disconnected from PLC. Retrying..."));
+plc.on("Disconnected", () => console.warn("Disconnected from PLC.. Reconnecting..."));
 plc.on("Error", (err) => console.error("PLC Error:", err));
 
 plc.connect();
+
+function SCADA_Toggle(plc) {
+    const tag = plc.tags.find(t => t.name === "Scada_Communication");
+
+    if (tag) {
+        tag.value = true;
+        setInterval(() => {
+            tag.value = !tag.value;
+        }, 500);
+        console.log("SCADA Comm bit toggling started");
+    } else {
+        console.warn("SCADA Comm bit not found");
+    }
+}
+
+SCADA_Toggle(plc);
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
--- tags.js
+++ tags.js
@@ -9,15 +9,15 @@ module.exports = {
         "Scale_In_Fail_Count", "Scale_In_Total_Count", "Station_1_ON", 
         "Station_2_ON", "Station_3_ON", "Station_4_ON", "Machine_In_Auto_Mode", 
         "Scada_Communication", "Scale_In_Data_Transfer_Bit", "Identifier", "Scada_Communication_Error"
-      ],
-      scale_out: [
-        "Scale_Out_Barcode", "Model_Name", "Acid_Temperature", "Station_No", 
-        "Scale_Out_Empty_Weight", "Scale_Out_Acid_Weight", "Scale_Out_Total_Weight", 
-        "Scale_Out_Result", "Scale_Out_Min_Limit", "Scale_Out_Max_Limit", 
-        "Scale_Out_Pass_Count", "Scale_Out_Fail_Count", "Scale_Out_Total_Count", 
-        "Station_1_ON", "Station_2_ON", "Station_3_ON", "Station_4_ON", 
-        "Machine_In_Auto_Mode", "Scada_Communication", "Scale_Out_Data_Transfer_Bit", 
-        "Identifier", "Scada_Communication_Error"
       ]
+      // scale_out: [
+      //   "Scale_Out_Barcode", "Model_Name", "Acid_Temperature", "Station_No", 
+      //   "Scale_Out_Empty_Weight", "Scale_Out_Acid_Weight", "Scale_Out_Total_Weight", 
+      //   "Scale_Out_Result", "Scale_Out_Min_Limit", "Scale_Out_Max_Limit", 
+      //   "Scale_Out_Pass_Count", "Scale_Out_Fail_Count", "Scale_Out_Total_Count", 
+      //   "Station_1_ON", "Station_2_ON", "Station_3_ON", "Station_4_ON", 
+      //   "Machine_In_Auto_Mode", "Scada_Communication", "Scale_Out_Data_Transfer_Bit", 
+      //   "Identifier", "Scada_Communication_Error"
+      // ]
     }
   }
